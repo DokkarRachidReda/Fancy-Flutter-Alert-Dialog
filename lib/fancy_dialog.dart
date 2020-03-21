6f858c3fadb146cdbecb6718680c2fed12d8e487
liudonghua123 <liudonghua123@gmail.com>
2020-03-21 22:25:13 +0800

    rewrite hardcoded diaglog width/height

--- lib/fancy_dialog.dart
+++ lib/fancy_dialog.dart
@@ -41,8 +41,6 @@ class GifDialogState extends State<FancyDialog> with TickerProviderStateMixin {
   AnimationController ac;
   Animation animation;
   double width;
-  double dwidth;
-  double dheight;
   double height;
   int animationAxis = 0; // 0 for x 1 for y
 
@@ -129,36 +127,21 @@ class GifDialogState extends State<FancyDialog> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     width = MediaQuery.of(context).size.width;
-    height = MediaQuery.of(context).size.height * 0.6;
-    dwidth = 0.59 * height;
-    dheight = 0.7 * height;
+    height = MediaQuery.of(context).size.height;
+    var dialogWidth = 0.36 * height;
 
-    var image;
-
-    if (package == 1) {
-      image = ClipRRect(
-        child: Image.asset(
-          gifPath,
-          fit: BoxFit.fill,
-          width: dwidth,
-          package: 'fancy_dialog',
-        ),
-        borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(theme == 0 ? 15 : 0),
-            topRight: Radius.circular(theme == 0 ? 15 : 0)),
-      );
-    } else {
-      image = ClipRRect(
-        child: Image.asset(
-          gifPath,
-          fit: BoxFit.fill,
-          width: dwidth,
-        ),
-        borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(theme == 0 ? 15 : 0),
-            topRight: Radius.circular(theme == 0 ? 15 : 0)),
-      );
-    }
+    var image = ClipRRect(
+      child: Image.asset(
+        gifPath,
+        fit: BoxFit.fill,
+        width: dialogWidth * 1.1, // dialogWidth will get left/right margin?
+        height: dialogWidth * 0.6,
+        package: package == 1 ? 'fancy_dialog' : null,
+      ),
+      borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(theme == 0 ? 15 : 0),
+          topRight: Radius.circular(theme == 0 ? 15 : 0)),
+    );
 
     return GestureDetector(
       onTap: () => {Navigator.of(context).pop()},
@@ -168,69 +151,61 @@ class GifDialogState extends State<FancyDialog> with TickerProviderStateMixin {
         ),
         elevation: 0.0,
         backgroundColor: Colors.transparent,
-        child: Center(
-          child: Container(
-            width: dwidth,
-            height: dheight,
-            transform: Matrix4.translationValues(
-                animationAxis == 0 ? animation.value * width : 0,
-                animationAxis == 1 ? animation.value * width : 0,
-                0),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(theme == 0 ? 15 : 0),
-              color: Colors.white,
-            ),
-            child: Container(
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.only(bottom: 5),
-                    height: 0.4 * dheight,
-                    child: image,
-                  ),
-                  Text(
-                    title,
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  Padding(
-                      padding: EdgeInsets.only(top: 0.08 * dwidth), //25
-                      child: Container(
-                        margin: EdgeInsets.only(left: 1),
-                        height: 0.28 * dheight,
-                        child: Text(
-                          descreption,
-                          style:
-                              TextStyle(color: Colors.grey[600], fontSize: 15),
-                          textAlign: TextAlign.center,
-                          maxLines: 4,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      )),
-                  Container(
-                    height: dheight * 0.15,
-                    margin: EdgeInsets.only(
-                        left: theme == 0 ? 0.075 * dwidth : 125), //20,40
-                    child: Row(
-                      children: <Widget>[
-                        theme == 0
-                            ? customButton(cancel, cancelColor, cancelFun)
-                            : flatButton(cancel, cancelColor, cancelFun),
-                        Padding(
-                          padding: EdgeInsets.only(
-                              left: theme == 0 ? 0.07 * dwidth : 1),
-                          child: theme == 0
-                              ? customButton(ok, okColor, okFun)
-                              : flatButton(ok, okColor, okFun),
-                        )
-                      ],
-                    ),
-                  )
-                ],
+        child: Container(
+          width: dialogWidth,
+          transform: Matrix4.translationValues(
+              animationAxis == 0 ? animation.value * width : 0,
+              animationAxis == 1 ? animation.value * width : 0,
+              0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(theme == 0 ? 15 : 0),
+            color: Colors.white,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              image,
+              Padding(
+                padding: const EdgeInsets.all(5),
+                child: Text(
+                  title,
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
               ),
-            ),
+              Padding(
+                  padding: EdgeInsets.fromLTRB(10, 5, 10, 5),
+                  child: Container(
+                    child: Text(
+                      descreption,
+                      style: TextStyle(color: Colors.grey[600], fontSize: 15),
+                      textAlign: TextAlign.center,
+                      maxLines: 5,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  )),
+              Container(
+                child: Row(
+                  mainAxisAlignment: theme == 1
+                      ? MainAxisAlignment.end
+                      : MainAxisAlignment.center,
+                  children: <Widget>[
+                    theme == 0
+                        ? customButton(cancel, cancelColor, cancelFun)
+                        : flatButton(cancel, cancelColor, cancelFun),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    theme == 0
+                        ? customButton(ok, okColor, okFun)
+                        : flatButton(ok, okColor, okFun)
+                  ],
+                ),
+              )
+            ],
           ),
         ),
       ),
@@ -239,11 +214,9 @@ class GifDialogState extends State<FancyDialog> with TickerProviderStateMixin {
 
   Widget customButton(String t, Color c, Function f) {
     return Container(
-      width: 0.4 * dwidth,
-      height: 0.15 * dheight,
       child: RaisedButton(
         shape: RoundedRectangleBorder(
-            borderRadius: new BorderRadius.circular(60.0)),
+            borderRadius: new BorderRadius.circular(30.0)),
         color: c,
         child: Text(
           t,
@@ -259,8 +232,6 @@ class GifDialogState extends State<FancyDialog> with TickerProviderStateMixin {
 
   Widget flatButton(String t, Color c, Function f) {
     return Container(
-      width: 0.29 * dwidth,
-      height: 0.15 * dheight,
       child: FlatButton(
         child: Text(
           t,
