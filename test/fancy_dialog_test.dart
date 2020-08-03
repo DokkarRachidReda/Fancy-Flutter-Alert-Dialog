5d80a7900b14e3d996d3c1d3229ee8778b3e26c6
DokkarRachidReda <hr_dokkar@esi.dz>
2020-08-03 03:35:17 +0100

    added first unit test

--- test/fancy_dialog_test.dart
+++ test/fancy_dialog_test.dart
@@ -1,9 +1,29 @@
+import 'dart:ui';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:fancy_dialog/FancyAnimation.dart';
+import 'package:fancy_dialog/FancyGif.dart';
+import 'package:fancy_dialog/FancyTheme.dart';
+import 'package:fancy_dialog/fancy_dialog.dart';
+void main(){
 
-class DialogTest{
+  
 
-  test('hello',(){
-    
-  expect(string.split(','), equals(['foo', 'bar', 'baz']));
-  });
+    testWidgets('show dialog test', (WidgetTester tester) async {
 
+        await tester.pumpWidget(
+              FancyDialog(
+                title: "Fancy Gif Dialog",
+                descreption:"This is descreption for fancy gif,you can load any image or gif to be displayed :), and you can choose between two themes Fancy and Flat",
+                animationType: FancyAnimation.BOTTOM_TOP,
+                theme: FancyTheme.FANCY,
+                gifPath: FancyGif.MOVE_FORWARD, //'./assets/walp.png',
+                okFun: (){print("it's working :)");},
+              )
+           );
+          
+           await tester.pump(Duration(seconds: 3));
+           expect(tester.takeException(),isAssertionError);
+
+     
+});
 }
